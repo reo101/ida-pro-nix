0decaca46be619ebee44500810ead83c591bdeea
reo101 <pavel.atanasov2001@gmail.com>
2026-05-05 22:06:48 +0300

    feat: initial
    - Nix flake based on `flake-parts` and `flake-file`
    - IDA Pro package
    - add MIT license
    
    Co-Authored-By: Dimitar Kolev <dimitarkolev@tutanota.com>

---
+++ nix/ida-pro.nix
@@ -0,0 +1,38 @@
+{ lib, ... }:
+
+{
+  perSystem =
+    { pkgs, system, ... }:
+    let
+      idaFhsEnv = pkgs.buildFHSEnv {
+        name = "ida-pro-fhs";
+        targetPkgs = pkgs: [ pkgs.python3 ];
+      };
+
+      libext = pkgs.stdenv.hostPlatform.extensions.sharedLibrary;
+    in
+    {
+      legacyPackages.ida-pro = (mkRawDerivation { }).extend (
+        final: _: {
+          inherit system;
+
+          name = "ida-pro";
+          version = "9.3.260327";
+
+          src = throw "Provide the source yourself, :クルーレス:";
+
+          builder = lib.getExe idaFhsEnv;
+          args = [
+            "-c"
+            /* bash */ ''
+              set -xeu pipefail
+
+              cp "$src" 'install.run';
+              chmod +x 'install.run';
+              ./'install.run' --mode 'unattended' --prefix "$out";
+            ''
+          ];
+        }
+      );
+    };
+}
