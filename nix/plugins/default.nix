2490c94b5a86965695ab887dda31500eb8832626
reo101 <pavel.atanasov2001@gmail.com>
2026-05-17 19:29:49 +0300

    feat(nix/plugins): expose under a `scope` (allow local overrides)

--- nix/plugins/default.nix
+++ nix/plugins/default.nix
@@ -1,13 +1,31 @@
+{ lib, ... }:
+
 {
   perSystem =
     { pkgs, ... }:
+    let
+      inherit (pkgs) lib;
+
+      scopeAttrs = scope: builtins.removeAttrs scope [
+        "callPackage"
+        "newScope"
+        "overrideScope"
+        "packages"
+      ];
+
+      pluginsScope = lib.makeScope pkgs.newScope (self: {
+        diaphora = self.callPackage ./diaphora.nix { };
+        hrtng = self.callPackage ./hrtng.nix { };
+        ida-cyberchef = self.callPackage ./ida-cyberchef.nix { };
+        ponce = self.callPackage ./ponce.nix { };
+        tenet = self.callPackage ./tenet.nix { };
+      });
+    in
     {
-      legacyPackages.plugins = {
-        diaphora = pkgs.callPackage ./diaphora.nix { };
-        hrtng = pkgs.callPackage ./hrtng.nix { };
-        ida-cyberchef = pkgs.callPackage ./ida-cyberchef.nix { };
-        ponce = pkgs.callPackage ./ponce.nix { };
-        tenet = pkgs.callPackage ./tenet.nix { };
+      legacyPackages = {
+        plugins = scopeAttrs pluginsScope;
+        pluginsScope = pluginsScope;
+        pluginsWith = overrides: scopeAttrs (pluginsScope.overrideScope overrides);
       };
     };
 }
