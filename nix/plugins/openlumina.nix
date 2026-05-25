505551f1eeeafef0dc412032b1ce00e25518aabc
Dimitar Kolev <dimitarkolev@tutanota.com>
2026-05-26 02:06:00 +0300

    feat(nix/plugins): package OpenLumina

---
+++ nix/plugins/openlumina.nix
@@ -0,0 +1,15 @@
+{
+  fetchzip,
+  ...
+}:
+
+rec {
+  pname = "openlumina";
+  version = "9.3.0";
+
+  src = fetchzip {
+    url = "https://github.com/tomrus88/OpenLumina/releases/download/v${version}/openlumina-ida9.3.zip";
+    stripRoot = false;
+    hash = "sha256-5riN3e89AV3Ulq7DXFe8WBX8C48D5DqncGX0m3o8YBc=";
+  };
+}
