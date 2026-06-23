98df482bdab68ef0d9ce87a727c60b2944f2215b
reo101 <pavel.atanasov2001@gmail.com>
2026-06-23 15:41:21 +0300

    feat(nix/plugins/hrtng): update to `3.9.105`

--- nix/plugins/hrtng.nix
+++ nix/plugins/hrtng.nix
@@ -16,20 +16,26 @@ let
     rev = "CRYPTOPP_8_9_0";
     hash = "sha256-HV+afSFkiXdy840JbHBTR8lLL0GMwsN3QdwaoQmicpQ=";
   };
+
+  cryptoppCmakeSrc = fetchFromGitHub {
+    owner = "abdes";
+    repo = "cryptopp-cmake";
+    rev = "d2b072ab65c036f3dca67f4204ad57d66728bf99";
+    hash = "sha256-FHHGq8hpLIsT6xq3hXY2n0wVFO4ahVXkSuWsKKnfn/U=";
+  };
 in
 rec {
   pname = "hrtng";
-  version = "3.9.0-dev";
+  version = "unstable-2026-07-07";
 
   drv = stdenv.mkDerivation rec {
     inherit pname version;
 
     src = fetchFromGitHub {
-      owner = "reo101";
+      owner = "KasperskyLab";
       repo = "hrtng";
-      rev = "fa74d36375e8aec3f9ef2ba3b745ea7212bb35b7";
-      fetchSubmodules = true;
-      hash = "sha256-wBamN8As8RTfYidHplqV4cOH80xHSW+najaTO9n3sIY=";
+      rev = "6202adf445b28f34945d6fe9397064d092e68867";
+      hash = "sha256-t1u4NxFO/crj/hUhJAWHgfOLROduV4Hz5Hwe18rs224=";
     };
 
     sourceRoot = "${src.name}/src";
@@ -40,6 +46,8 @@ rec {
     ];
 
     postPatch = ''
+      rm -rf cryptopp-cmake
+      ln -s ${cryptoppCmakeSrc} cryptopp-cmake
       ln -s ${cryptoppSrc} cryptopp
       substituteInPlace helpers.cpp \
         --replace-fail 'add_extra_cmt(ea, true, cmt.c_str());' 'add_extra_cmt(ea, true, "%s", cmt.c_str());'
@@ -50,7 +58,6 @@ rec {
     '';
 
     cmakeFlags = [
-      # (lib.cmakeOptionType "PATH" "CRYPTOPP_SOURCES" "${cryptoppSrc}")
       (lib.cmakeFeature "CMAKE_BUILD_TYPE" "Release")
       (lib.cmakeFeature "IDASDK_VER" (lib.versions.pad 2 ida-pro-version))
       (lib.cmakeFeature "IDASDK_DIR" "${ida-sdk}/src")
